9ea0775d27cf0747b85f599de9d147c9be453b1c
simov <simeonvelichkov@gmail.com>
2016-06-30 19:47:24 +0300

    Refactor the discovery module

--- server/discovery.js
+++ server/discovery.js
@@ -4,14 +4,16 @@ var rb = require('./rubygems')
 
 
 exports.run = (root, step, done) => {
-  if (done === undefined) {
-    done = step
-    step = null
-  }
-
   var gems = {}
-  var wait = {}
   var error = false
+
+  getGem({name: root}, (err) => {
+    if (err) {
+      done(err)
+      return
+    }
+    done(null, gems)
+  })
 
   function getGem (gem, done) {
     if (error) {
@@ -19,61 +21,44 @@ exports.run = (root, step, done) => {
       return
     }
 
-    if (!gems.hasOwnProperty(gem.name) && !wait.hasOwnProperty(gem.name)) {
-      wait[gem.name] = true
-      rb.get('gems/' + gem.name, (err, res) => {
-        if (err) {
-          done(err)
-          return
-        }
-
-        delete wait[gem.name]
-        gems[gem.name] = res.body
-
-        // console.log('gem', '->', gem.name)
-        if (step) {
-          error = step(res.body)
-        }
-        if (error) {
-          done(error)
-          return
-        }
-
-        if (res.body.dependencies.runtime.length) {
-          deps(res.body.dependencies.runtime, done)
-        }
-        else {
-          done()
-        }
-      })
-    }
-    else {
-      // console.log('pass')
+    // skip
+    if (gems[gem.name]) {
       done(error)
+      return
     }
+
+    gems[gem.name] = true
+
+    // get
+    rb.get('gems/' + gem.name, (err, res) => {
+      if (err) {
+        done(err)
+        return
+      }
+
+      gems[gem.name] = res.body
+
+      if (step) {
+        error = step(res.body)
+      }
+      // client has disconnected
+      if (error) {
+        done(error)
+        return
+      }
+
+      // deps
+      if (res.body.dependencies.runtime.length) {
+        deps(res.body.dependencies.runtime, done)
+      }
+      // bottom
+      else {
+        done()
+      }
+    })
   }
 
   function deps (dependencies, done) {
-    async.each(
-      dependencies,
-      getGem,
-      (err) => {
-        if (err) {
-          done(err)
-          return
-        }
-        // console.log('DONE!')
-        done()
-      }
-    )
+    async.each(dependencies, getGem, done)
   }
-
-  getGem({name: root}, (err) => {
-    if (err) {
-      done(err)
-      return
-    }
-    // console.log('FINISH!')
-    done(null, gems)
-  })
 }
