b0dbed79da9ef5304f305527e55930cc99b5df32
simov <simeonvelichkov@gmail.com>
2016-06-30 17:39:04 +0300

    Leave only the link to my profile in the footer

--- mithril/footer.js
+++ mithril/footer.js
@@ -2,10 +2,5 @@
 var Footer = {
   view: () =>
     m('footer',
-      m('p', [
-        m('a[href="http://simov.github.io"][target="_blank"]', 'simov'),
-        ' ↔ ',
-        m('a[href="https://github.com/simov/rubinho"][target="_blank"]', 'rubinho')
-      ])
-    )
+      m('p', m('a[href="http://simov.github.io"][target="_blank"]', 'simov')))
 }
